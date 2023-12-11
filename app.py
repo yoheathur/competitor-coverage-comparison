0f71f7a7c17a3476949f4725f83f5a071ad68d34
Heather Williams <heather.williams@amberdata.io>
2023-12-11 20:26:38 +0100

    Edited code to parse the JSON response data

--- app.py
+++ app.py
@@ -8,17 +8,21 @@ def get_data():
     # Replace with your private API URL and required headers
     api_url = 'https://web3api.io/api/v2/market/exchanges'
     headers = {
-        'x-api-key': 'UAK5b2464bd90aa63316784d05427a042f7',# Replace with your API token
+        'x-api-key': 'UAK5b2464bd90aa63316784d05427a042f7',  # Replace with your API token
         'accept': 'application/json'
         # Include other necessary headers
     }
 
     response = requests.get(api_url, headers=headers)
     if response.status_code == 200:
-        return jsonify(response.json())
+        data = response.json()
+
+        # Extracting the exchange names
+        exchange_names = list(data.get('payload', {}).get('data', {}).keys())
+
+        return jsonify(exchange_names)
     else:
         return jsonify({"error": "Failed to fetch data"}), response.status_code
 
 if __name__ == '__main__':
     app.run(port=5001)
-
